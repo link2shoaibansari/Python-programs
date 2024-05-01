203a3b96e53d013cb287c5538c68e6ec902168dd
Md Shoaib Ansari <116276693+link2shoaibansari@users.noreply.github.com>
2024-05-01 13:00:16 +0530

    Add files via upload

---
+++ 01-avgoftwonumbers.py
@@ -0,0 +1,5 @@
+# to find average of two numbers entered by the user
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
+avg = (a+b)/2
+print("Average of",a,"and",b,"is: ",avg)
