604be14fcc520d8faf9d652ea7c6d6f8c994a6f0
Md Shoaib Ansari <116276693+link2shoaibansari@users.noreply.github.com>
2024-05-04 14:30:00 +0530

    Add files via upload

---
+++ 06-strings_in_python.py
@@ -0,0 +1,8 @@
+# to print different string characters
+name = "Shoaib"
+print(name)
+ch = str(input("Enter some characters: "))
+print(ch)
+strr = '''He Said- "Hehehehehe Potty!!!"'''
+for character in strr:
+    print(character)
---
+++ 07-string_slicing.py
@@ -0,0 +1,6 @@
+# string slicing in python
+
+name = str(input("Enter your name: "))
+print("Your name has",len(name),"letters.")
+name2 = "Shoaib"
+print(name2[-4:-2])
